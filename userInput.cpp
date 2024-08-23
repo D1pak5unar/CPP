0e3d159d68d9b531d2b834b3983dff45fbbb7bdf
D1pak5unar <160437980+D1pak5unar@users.noreply.github.com>
2024-08-22 22:52:44 -0500

    User Input in c++ with cin

--- userInput.cpp
+++ userInput.cpp
@@ -1,1 +1,20 @@
-//start from 44 min 49 sec on the video by Caleb Curry
+//getting user input 
+
+#include <iostream>
+
+int main()
+{
+    // int randomNumber;
+
+
+    int slices;
+    std::cout << "Howdy!! How many slices of pizza do you want?: ";
+
+    std::cin >> slices;
+    std::cout << "You have " << slices << " slices of pizza."<< std::endl;
+
+
+    // std::cin >> randomNumber;
+    // std::cout << "The Number is " << randomNumber << std::endl;
+    return 0; 
+}
