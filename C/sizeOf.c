02073f68d3042b8ec7d0557b25474dbae34e9732
D1pak5unar <160437980+D1pak5unar@users.noreply.github.com>
2024-08-27 21:22:22 -0500

    Print the size of the operators

---
+++ C/sizeOf.c
@@ -0,0 +1,18 @@
+// Printing the size of the operator 
+#include <stdio.h>
+
+int main()
+{
+    int myInt;
+    float myFloat;
+    double myDouble;
+    char myChar;
+
+    printf("%lu\n", sizeof(myInt));
+    printf("%lu\n", sizeof(myFloat));
+    printf("%lu\n", sizeof(myDouble));
+    printf("%lu", sizeof(myChar));
+
+    return 0;
+
+}
