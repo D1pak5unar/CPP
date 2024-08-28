561a25b2d3299ada2fa3f9bd8613044b9e01441f
D1pak5unar <160437980+D1pak5unar@users.noreply.github.com>
2024-08-27 20:41:46 -0500

    Example with student info

---
+++ C/studentInfo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    //student data
+    int studentID = 90;
+    int studentAge = 19;
+    float studentFee = 98.99;
+    int studentGrade = 'B';
+
+    // Print Variables.
+    printf("Student id: %d\n", studentID);
+    printf("Student Age: %d\n", studentAge);
+    printf("Student Fee: %f\n", studentFee);
+    printf("Student Grade: %c", studentGrade);
+
+    return 0;
+}
